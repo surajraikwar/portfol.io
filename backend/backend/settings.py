c55c2048ea27302d72e6cb11a21aec54a0339f66
Suraj <surajkashyap1996@gmail.com>
2020-07-13 14:50:18 +0530

    configured app for heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_URL = 'knowme/user_login'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
