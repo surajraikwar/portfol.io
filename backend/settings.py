549c0e2559df0e6225d6f2dc2e7651a93bb2d36a
Suraj <surajkashyap1996@gmail.com>
2020-07-16 12:32:06 +0530

    confuguring wkhtmltopdf

--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'knowme'
+    'knowme',
+    'wkhtmltopdf'
 ]
 AUTH_USER_MODEL = 'knowme.Account'
 
